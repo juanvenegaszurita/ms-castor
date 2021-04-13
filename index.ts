49990272bbd142c5678173d8f3d11cffd04814ab
Juan Venegas Zurita <jvenegas@bancoestado.cl>
2021-04-13 14:36:49 -0400

    console log

--- index.ts
+++ index.ts
@@ -16,7 +16,9 @@ class Server {
     try {
       const firebaseConfig = environment.FIREBASE_CASTOR;
       console.log("firebaseConfig", firebaseConfig);
-      initializeApp(JSON.parse(firebaseConfig));
+      const firebaseConfigJSOn = JSON.parse(firebaseConfig);
+      console.log("firebaseConfigJSOn", firebaseConfigJSOn)
+      initializeApp(firebaseConfigJSOn);
       console.log("Firebase OK");
     } catch (error) {
       console.log("Firebase ERROR", error);
