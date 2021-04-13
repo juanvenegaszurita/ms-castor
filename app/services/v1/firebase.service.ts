4e5c3920e4385eb8ff93565eb35b5af08e1843c5
Juan Venegas Zurita <jvenegas@bancoestado.cl>
2021-04-13 14:16:50 -0400

    se cambia la integración de firebase

--- app/services/v1/firebase.service.ts
+++ app/services/v1/firebase.service.ts
@@ -1,20 +1,11 @@
-import { environment } from '../../../environments/environment';
 import firebase from 'firebase-admin'
 
 export class FirebaseService {
 
-  initFirebase(nameConexion: string = "ms-castor-firebase"): firebase.app.App {
-    try {
-      return firebase.initializeApp( JSON.parse(environment.FIREBASE_CASTOR), nameConexion );
-    } catch (error) {
-      return firebase.app(nameConexion);
-    }
-  }
   async validateToken(token: string): Promise<firebase.auth.DecodedIdToken | null> {
-    const nameConexion = await this.initFirebase();
     let userCredential: firebase.auth.DecodedIdToken | null = null;
     try {
-      userCredential = await nameConexion.auth().verifyIdToken(token);
+      userCredential = await firebase.auth().verifyIdToken(token);
     } catch (error) {
       console.log("Error => validateToken", error);
     }
--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import { App } from './app/app';
 import { IndexBD } from './app/bd/index.db';
+import { initializeApp, credential } from 'firebase-admin';
+import { environment } from './environments/environment';
 
 class Server {
   app: express.Application = express();
 
   constructor() {
     IndexBD.init();
+    this.firebaseAdmin();
     this.createServer();
+  }
+  private firebaseAdmin() {
+    try {
+      initializeApp(JSON.parse(environment.FIREBASE_CASTOR));
+      console.log("Firebase OK");
+    } catch (error) {
+      console.log("Firebase ERROR", error);
+    }
   }
   createServer() {
     this.app.use( App.getApp() );
